53a17ccf9ba305dfb7ea4f9cd697962a8c4c234e
Jack Durning <jackdurning@Jacks-MacBook-Pro.local>
2022-01-10 13:47:58 -0500

    minerphase

--- src/VVacked/Archon.java
+++ src/VVacked/Archon.java
@@ -1,5 +1,15 @@
 package VVacked;
 
 public class Archon {
-    
+    public static int minerPhaseEnd = 100;
+
+    public static void run(RobotController rc) throws GameActionException {
+        
+    }
+
+    public static void SpawnPhaseLogic(RobotController rc) {
+        if (rc.turnCount < minerEndPhase) {
+            
+        }
+    }
 }
