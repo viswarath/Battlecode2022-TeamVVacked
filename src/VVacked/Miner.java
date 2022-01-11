78dcc510b702bcc931746a4e98f4dd1ea1cf30a4
Viswa Rathnakumar <Vsocc@Viswas-MacBook-Pro.local>
2022-01-10 20:04:39 -0500

    idvk

--- src/VVacked/Miner.java
+++ src/VVacked/Miner.java
@@ -5,20 +5,21 @@ import battlecode.common.*;
 public class Miner {
     //direction of movement
     public static Direction move = Direction.CENTER;
-    public static boolean foundLocation = false;
+    public static boolean foundLeadLocation = false;
     public MapLocation targetLocation;
 
     public static void run(RobotController rc) throws GameActionException{
-        if (!foundLocation){
+        if (!foundLeadLocation){
             MapLocation[] leadLocations = rc.senseNearbyLocationsWithLead();
             for (MapLocation loc : leadLocations){
                 if ((rc.senseLead(loc) > 1) && (rc.senseRobotAtLocation(loc) == null)){
                     RobotInfo[] robots = rc.senseNearbyRobots();
+                    int five = 5;
                 }
             }
         }
 
-        if (rc.canMove(move) && !foundLocation){
+        if (rc.canMove(move) && !foundLeadLocation){
             rc.move(move);
         }
     }
