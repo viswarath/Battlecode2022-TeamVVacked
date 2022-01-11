634789a093a7b8f472bd35d906e7e5f32820c258
Jack Durning <jackdurning@Jacks-MacBook-Pro.local>
2022-01-10 19:05:42 -0500

    minerLogic

--- src/VVacked/Miner.java
+++ src/VVacked/Miner.java
@@ -3,13 +3,31 @@ package VVacked;
 import battlecode.common.*;
 
 public class Miner {
+    //direction of movement
+    public Direction move = Direction.CENTER;
+    public boolean foundLocation = false;
+    public MapLocation targetLocation;
+
     public static void run(RobotController rc) throws GameActionException{
+        if (!foundLocation){
+            MapLocation[] leadLocations = rc.senseNearbyLocationsWithLead(2);
+            for (MapLocation loc : leadLoactions){
+                if (rc.senseLead(loc) > 1){
+                    RobotInfo[] robots = rc.senseNearbyRobots();
+                }
+            }
+        }
+
+        if (rc.canMove(move) && !foundLocation){
+            rc.move(move);
+        }
+    }
+
+    public static void init(RobotController rc) throwsa GameActionException{
         //nearby bots
         RobotInfo[] robots = rc.senseNearbyRobots();
         //current MapLocation
         MapLocation loc = rc.getLocation();
-        //direction of movement
-        Direction move = Direction.CENTER;
 
         for (RobotInfo robot : robots){
             if (robot.getTeam() == rc.getTeam() && robot.getType() == RobotType.ARCHON){
@@ -17,10 +35,5 @@ public class Miner {
                 move = base.directionTo(loc);
             }
         }
-
-        if (rc.canMove(move)){
-            rc.move(move);
-        }
-
     }
 }
