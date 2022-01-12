b3ac44baa562e12252440430cb15bffd8a0709d2
Jack Durning <jackdurning@Jacks-MacBook-Pro.local>
2022-01-12 15:29:33 -0500

    Holy shit the miners finally work decently

--- src/VVacked/Miner.java
+++ src/VVacked/Miner.java
@@ -7,42 +7,83 @@ public class Miner {
     public static Direction move = Direction.CENTER;
     public static boolean foundLeadLocation = false;
     public static MapLocation targetLocation;
+    public static boolean closeMiner = false;
+    public static boolean reverseMiner = false;
+    public static int directionIndex = 0;
 
     public static void run(RobotController rc) throws GameActionException{
         MapLocation[] leadLocations = rc.senseNearbyLocationsWithLead();
+        // if (closeMiner){
+        //     leadLocations = rc.senseNearbyLocationsWithLead(2);
+        // } else{
+        //     leadLocations = rc.senseNearbyLocationsWithLead();
+        // }
 
-        if (!rc.onTheMap(rc.getLocation().add(move))) {
-            move = move.opposite().rotateRight();
+        if (reverseMiner){
+            for(int i = 0; i < leadLocations.length / 2; i++)
+            {
+                MapLocation temp = leadLocations[i];
+                leadLocations[i] = leadLocations[leadLocations.length - i - 1];
+                leadLocations[leadLocations.length - i - 1] = temp;
+            }
         }
         
+        if (!rc.onTheMap(rc.getLocation().add(move))) {
+            move = move.opposite().rotateRight();
+            forLoop:
+            for (int i = 0; i < Data.directions.length; i++){
+                if (Data.directions[i] == move){
+                    directionIndex = i;
+                    break forLoop;
+                }
+            }
+        }
+
         if (!foundLeadLocation){
+            Direction[] facingArray = Pathfinding.getFacingArray(rc, directionIndex);
             forLoop:
             for (MapLocation loc : leadLocations){
                 if ((rc.senseLead(loc) > 1) && (rc.senseRobotAtLocation(loc) == null)){
-                    targetLocation = loc;
-                    foundLeadLocation = true;
-                    break forLoop;              
+                    for (Direction dir : facingArray){
+                        if (rc.getLocation().directionTo(loc) == dir){
+                            targetLocation = loc;
+                            System.out.println("(" + targetLocation.x + "' " + targetLocation.y + "), " + rc.getID());
+                            foundLeadLocation = true;
+                            break forLoop; 
+                        }
+                    }             
                 }
             }
         }
 
         Direction dir = Pathfinding.basicMove(rc, targetLocation);
-        if (rc.canMineLead(targetLocation) && rc.senseLead(targetLocation) > 1){
-            while (rc.senseLead(targetLocation) > 1 && rc.canMineLead(targetLocation)){
-                foundLeadLocation = false;
-                rc.mineLead(targetLocation);
-            }
-        } else if (rc.canMove(dir) && foundLeadLocation){
-            rc.move(dir);   
-        } 
-        else if (rc.canMove(move)){
-            forLoop:
-            for (int i = 0; i < Data.directions.length; i++){
-                if (Data.directions[i] == move){
-                    rc.move(Pathfinding.getSemiRandomDir(rc, i+1));
-                    break forLoop;
+        while (rc.senseLead(targetLocation) > 1 && rc.canMineLead(targetLocation)){
+            MapLocation me = rc.getLocation();
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                    // Notice that the Miner's action cooldown is very low.
+                    // You can mine multiple times per turn!
+                    while (rc.canMineGold(mineLocation)) {
+                        rc.mineGold(mineLocation);
+                    }
+                    while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > 1) {
+                        rc.mineLead(mineLocation);
+                    }
                 }
             }
+            if (rc.senseLead(targetLocation) < 2){
+                foundLeadLocation = false;
+            }
+        }
+        if (rc.canSenseLocation(targetLocation)){
+            if (rc.canMove(dir) && foundLeadLocation && !rc.getLocation().isAdjacentTo(targetLocation)){
+                rc.move(dir);   
+            } else if (rc.senseLead(targetLocation) < 2 && rc.canMove(move)){
+                rc.move(Pathfinding.getSemiRandomDir(rc, directionIndex));
+            }
+        } else{
+            foundLeadLocation = false;
         }
     }
 
@@ -54,10 +95,36 @@ public class Miner {
         //set default target
         targetLocation = loc;
 
+        MapLocation baseLoc = rc.getLocation();
+
         for(RobotInfo robot: robots){
             if (robot.getType() == RobotType.ARCHON && robot.getTeam() == rc.getTeam()){
                 MapLocation base = robot.getLocation();
+                baseLoc = base;
                 move = base.directionTo(loc);
+            }
+        }
+
+        int rand = Data.rng.nextInt(3);
+        if (rand == 0){
+            closeMiner = true;
+        }
+        rand = Data.rng.nextInt(3);
+        if (baseLoc.x < rc.getMapWidth()/2.0){
+            if (rand == 0){
+                reverseMiner = true;
+            }
+        } else{
+            if (rand != 0){
+                reverseMiner = true;
+            }
+        }
+
+        forLoop:
+        for (int i = 0; i < Data.directions.length; i++){
+            if (Data.directions[i] == move){
+                directionIndex = i;
+                break forLoop;
             }
         }
     }
--- src/VVacked/Pathfinding.java
+++ src/VVacked/Pathfinding.java
@@ -49,56 +49,56 @@ public class Pathfinding {
     public static Direction getSemiRandomDir(RobotController rc, int baseDir) throws GameActionException {
         int num = Data.rng.nextInt(3);
         switch (baseDir) {
+            case 0:
+                if (num==1)
+                    return Direction.NORTHWEST;
+                else if (num==2)
+                    return Direction.NORTH;
+                else 
+                    return Direction.NORTHEAST;
             case 1:
                 if (num==1)
-                    return Direction.NORTHWEST;
-                else if (num==2)
                     return Direction.NORTH;
-                else 
+                else if (num==2)
                     return Direction.NORTHEAST;
+                else 
+                    return Direction.EAST;
             case 2:
                 if (num==1)
-                    return Direction.NORTH;
-                else if (num==2)
-                    return Direction.NORTHEAST;
-                else 
-                    return Direction.EAST;
-            case 3:
-                if (num==1)
                     return Direction.NORTHEAST;
                 else if (num==2)
                     return Direction.EAST;
                 else 
                     return Direction.SOUTHEAST;
-            case 4:
+            case 3:
                 if (num==1)
                     return Direction.EAST;
                 else if (num==2)
                     return Direction.SOUTHEAST;
                 else 
                     return Direction.SOUTH;
-            case 5:               
+            case 4:               
                 if (num==1)
                     return Direction.SOUTHEAST;
                 else if (num==2)
                     return Direction.SOUTH;
                 else 
                     return Direction.SOUTHWEST;
-            case 6:
+            case 5:
                 if (num==1)
                     return Direction.SOUTH;
                 else if (num==2)
                     return Direction.SOUTHWEST;
                 else 
                     return Direction.WEST;
-            case 7:
+            case 6:
                 if (num==1)
                     return Direction.SOUTHWEST;
                 else if (num==2)
                     return Direction.WEST;
                 else 
                     return Direction.NORTHWEST;
-            case 8:
+            case 7:
                 if (num==1)
                     return Direction.WEST;
                 else if (num==2)
@@ -107,5 +107,27 @@ public class Pathfinding {
                     return Direction.NORTH;
         }
         return Direction.CENTER;
+    }
+
+    public static Direction[] getFacingArray(RobotController rc, int baseDir) throws GameActionException {
+        switch (baseDir) {
+            case 0:
+                return new Direction[] {Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST};
+            case 1:
+                return new Direction[] {Direction.NORTH, Direction.NORTHEAST, Direction.EAST};
+            case 2:
+                return new Direction[] {Direction.NORTHEAST, Direction.EAST, Direction.SOUTHEAST};
+            case 3:
+                return new Direction[] {Direction.EAST, Direction.SOUTHEAST, Direction.SOUTH};
+            case 4:               
+                return new Direction[] {Direction.SOUTHEAST, Direction.SOUTH, Direction.SOUTHWEST};
+            case 5:
+                return new Direction[] {Direction.SOUTH, Direction.SOUTHWEST, Direction.WEST};
+            case 6:
+                return new Direction[] {Direction.SOUTHWEST, Direction.WEST, Direction.NORTHWEST};
+            case 7:
+                return new Direction[] {Direction.WEST, Direction.NORTHWEST, Direction.NORTH};
+        }
+        return new Direction[] {Direction.CENTER};
     }
 }
