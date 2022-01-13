9ce98c6f540901f336643466a3bcb2aa8d149fee
Viswa Rathnakumar <Vsocc@Viswas-MacBook-Pro.local>
2022-01-13 12:30:12 -0500

    beans

--- src/VVacked/Archon.java
+++ src/VVacked/Archon.java
@@ -25,16 +25,20 @@ public class Archon {
          * can only take place on round two because the archons need the 1st round to input guessed locations
         **/
         if (rc.getRoundNum() == 2){
-            MapLocation check = null;
             for (int i = 0;i < 12; i++){
+                MapLocation check = null;
                 int intLoc = rc.readSharedArray(i);
                 if (intLoc != 0){
-                    check = Data.readMapLocationFromSharedArray(rc, rc.readSharedArray(i));
+                    check = Data.readMapLocationFromSharedArray(rc, intLoc);
                     if(rc.canSenseLocation(check)){
-                        if(rc.canSenseRobotAtLocation(check) && rc.senseRobotAtLocation(check).getTeam() != rc.getTeam()){
+                        if(rc.canSenseRobotAtLocation(check)){
+                            if(rc.senseRobotAtLocation(check).getTeam() == rc.getTeam()){
+                                rc.writeSharedArray(i, 0);
+                            }
+                        } else{
                             rc.writeSharedArray(i, 0);
                         }
-                    }
+                    } 
                 }
             }
         }
@@ -98,7 +102,7 @@ public class Archon {
         int reflectedX = (rc.getMapWidth()-1) - rc.getLocation().x;
         int reflectedY = (rc.getMapHeight()-1) - rc.getLocation().y;          
         rc.writeSharedArray(0 + buffer, reflectedX*100 + rc.getLocation().y);
-        rc.writeSharedArray(1 + buffer, rc.getLocation().x + reflectedY);
+        rc.writeSharedArray(1 + buffer, (rc.getLocation().x)*100 + reflectedY);
         rc.writeSharedArray(2 + buffer, reflectedX*100 + reflectedY);
     }
 
@@ -143,6 +147,10 @@ public class Archon {
             addPossibleEnemyArchonLocations(rc, 6);
         } else{
             addPossibleEnemyArchonLocations(rc, 9);
+        }
+        System.out.print(rc.getID());
+        for(int i = 0;i <12; i++){
+            System.out.print("( " +rc.readSharedArray(i) + " )");
         }
     }
 }
