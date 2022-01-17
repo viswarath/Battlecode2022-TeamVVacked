da946cda180e2cfff95bab678ce637e872e086b4
Viswa Rathnakumar <Vsocc@Viswas-MacBook-Pro.local>
2022-01-17 14:24:09 -0500

    merge

--- src/ExplosiveTurtle/Archon.java
+++ src/ExplosiveTurtle/Archon.java
@@ -4,33 +4,46 @@ import battlecode.common.*;
 
 public class Archon {
 
-    public static int radialDirectionIndex = 0;
-    public static int firstMinerPhaseEnd = 16;
-    public static int minersSpawned = 0;
+    //public static int radialDirectionIndex = 0; //index to help spawn around in a circle
+    public static int minersSpawned = 0; //number of miners spawned
+    public static int robotsSpawned = 0; //number of robots spawned in miner phase
+    public static int minerRatio = 3; //spawn miners every this number of soldiers
 
     public static boolean enemyArchonNearby = false;
     public static boolean checkedGuessedLocs = false;
     public static MapLocation nearbyEnemyArchonLocation;
 
-    public static int defaultMinerNumber = 2;
-    public static int maxMinerSpawns = 16;
-    public static int nearbyLeadLocations = 0;
+    public static int defaultMinerNumber = 4; //minimum number of miners to spawn
+    public static int maxMinerSpawns = 11; //max number of miners to spawn
+    public static int nearbyLeadLocations = 0; //number of lead deposits nearby
 
-    public static int maxSoldierSpawns = 15;
-    public static int soldiersSpawned = 0;
+    //explosive turtle shit
+    public static int soldiersInCircle = 15;
+    public static boolean startCooldown = false;
+    public static int lookForCircleCooldown = 2;
 
-    public static int maxBuilderSpawns = 1;
-    public static int builderSpawned = 0;
+    public static int leadBeforeBuild;
+
+    public static int soldiertoMinerCount = 0;
+    public static int soldierToMinerRatio = 3;
+    public static boolean minerPhaseEnd = false;
+
+    //public int minerSpawnIndex
     
     public static void run(RobotController rc) throws GameActionException{
-
-        Direction build = Direction.CENTER;
 
         /**
          * delete nearby guessed locations in the shared array
          * different from checking around for enemy locations this takes less precendence
          * can only take place on round three because the archons need the second round to input guessed locations
         **/
+        if (enemyArchonNearby){
+            if( rc.canBuildRobot(RobotType.SOLDIER,Pathfinding.basicBuild(rc, nearbyEnemyArchonLocation, RobotType.SOLDIER))){
+                rc.buildRobot(RobotType.SOLDIER,Pathfinding.basicBuild(rc, nearbyEnemyArchonLocation, RobotType.SOLDIER));
+            }else{
+                return;
+            }
+        }
         if (!checkedGuessedLocs && rc.getRoundNum() > 2){
             for (int i = 0;i < 12; i++){
                 MapLocation check = null;
@@ -45,41 +58,103 @@ public class Archon {
                         } else{
                             rc.writeSharedArray(i, 0);
                         }
-                    }
+                    } 
                 }
             }
             checkedGuessedLocs = true;
             //print to check updated locations
-            System.out.print(rc.getID());
-            for(int i = 0;i <12; i++){
-                System.out.print("( " +rc.readSharedArray(i) + " )");
+            // System.out.print(rc.getID());
+            // for(int i = 0;i <12; i++){
+            //     System.out.print("( " +rc.readSharedArray(i) + " )");
+            // }
+        }
+
+        //EXPLOSIVE TURTLE CHECK
+        RobotInfo[] robots = rc.senseNearbyRobots();
+        int soldiersNearby = 0;
+        for (RobotInfo robot : robots){
+            if (robot.type == RobotType.SOLDIER && robot.getTeam() == rc.getTeam()){
+                soldiersNearby++;
+            }
+        }
+        if (soldiersNearby >= soldiersInCircle){
+            forLoop:
+            for (int i = 56; i < 63; i+=2){
+                if (rc.readSharedArray(i) == rc.getID()){
+                    rc.writeSharedArray(i+1, 1);
+                    break forLoop;
+                }
+            }
+            startCooldown = true;
+        }
+        if (startCooldown){
+            lookForCircleCooldown--;
+            if (lookForCircleCooldown == 0){
+                startCooldown = false;
+                lookForCircleCooldown = 2;
+                forLoop:
+                for (int i = 56; i < 63; i+=2){
+                    if (rc.readSharedArray(i) == rc.getID()){
+                        rc.writeSharedArray(i+1, 0);
+                        break forLoop;
+                    }
+                }
             }
         }
 
-        build = getRadialSpawnDir(rc, RobotType.MINER);
-        if (minersSpawned < maxMinerSpawns && rc.canBuildRobot(RobotType.MINER, build)){
-            rc.buildRobot(RobotType.MINER, build);
-            minersSpawned += 1;
-        } else if(soldiersSpawned < maxSoldierSpawns && rc.canBuildRobot(RobotType.SOLDIER, build)){
-            rc.buildRobot(RobotType.SOLDIER, build);
-            soldiersSpawned+= 1;
-        } else if (builderSpawned < maxBuilderSpawns && rc.getRoundNum() > 500){
-            rc.buildRobot(RobotType.BUILDER, build);
-            builderSpawned += 1;
+        //checks lead available
+        if (rc.getTeamLeadAmount(rc.getTeam()) > leadBeforeBuild){
+            rc.writeSharedArray(40, 1);
+        } else if (rc.getTeamLeadAmount(rc.getTeam()) < 16){
+            rc.writeSharedArray(40, 0);
         }
 
+        //gets build direction and builds
+        BuildLogic(rc);
+    }
+
+    private static void BuildLogic(RobotController rc) throws GameActionException {
+        Direction build;
+        if (minersSpawned < maxMinerSpawns){
+            if (soldiertoMinerCount%soldierToMinerRatio == 0){
+                build = getMinerSpawnDir(rc);
+            } else{
+                build = getSoldierSpawnDir(rc);
+            }
+        } else{
+            checkForMinerPhaseEnd(rc);
+            if (robotsSpawned%minerRatio != 0){
+                build = getSoldierSpawnDir(rc);
+            } else{
+                build = getMinerSpawnDir(rc);
+            }
+        }
+        //actually builds
         if (build != Direction.CENTER){
-            radialDirectionIndex+=1;
-            if (radialDirectionIndex == Data.directions.length){
-                radialDirectionIndex = 0;
-            }
-        }
-
-        for (RobotInfo robot: rc.senseNearbyRobots(20, rc.getTeam())){
-            if(robot.getHealth() < 30 && robot.getType() == RobotType.SOLDIER){
-                if (rc.canRepair(robot.getLocation())){
-                    rc.repair(robot.getLocation());
-                    System.out.print("repairing");
+            if (minersSpawned < maxMinerSpawns){
+                if (soldiertoMinerCount%soldierToMinerRatio != 0){
+                    if (rc.canBuildRobot(RobotType.MINER, build)){
+                        rc.buildRobot(RobotType.MINER, build);
+                        System.out.println("SPAWNING MINER IN MINER PHASE");
+                        minersSpawned++;
+                        soldiertoMinerCount++;
+                    }
+                } else{
+                    if (rc.canBuildRobot(RobotType.SOLDIER, build)){
+                        rc.buildRobot(RobotType.SOLDIER, build);
+                        System.out.println("SPAWNING SOLDIER IN MINER PHASE");
+                        soldiertoMinerCount++;
+                    }
+                }
+                
+            } else if(rc.readSharedArray(40) == 1){
+                if (robotsSpawned%minerRatio != 0){
+                    rc.buildRobot(RobotType.SOLDIER, build);
+                    robotsSpawned+=1;
+                } else{
+                    rc.buildRobot(RobotType.MINER, build);
+                    minersSpawned++;
+                    robotsSpawned+=1;
                 }
             }
         }
@@ -88,7 +163,7 @@ public class Archon {
     public static Direction getMinerSpawnDir(RobotController rc) throws GameActionException{
         //RobotType type = RobotType.MINER;
         MapLocation[] leadLocations = rc.senseNearbyLocationsWithLead();
-        MapLocation maxLeadLocation = null;
+        MapLocation maxLeadLocation = new MapLocation(rc.getLocation().x+1, rc.getLocation().y);
         for (int i = 0; i < leadLocations.length; i++){
             if (maxLeadLocation == null){
                 maxLeadLocation = leadLocations[i];
@@ -97,26 +172,42 @@ public class Archon {
                     maxLeadLocation = leadLocations[i];
                 }
             }
-            System.out.print(maxLeadLocation);
         }
-        return Pathfinding.basicBuild(rc, maxLeadLocation, RobotType.MINER);
+
+        Direction dir = Pathfinding.basicBuild(rc, maxLeadLocation, RobotType.MINER);
+        //if (minersSpawned % 3 != 0){
+            return dir;
+        //} else{
+
+        //}
     }
 
-
-    public static Direction getRadialSpawnDir(RobotController rc, RobotType type) throws GameActionException{
-        for (int i = radialDirectionIndex; i < Data.directions.length; i++){
-            if (rc.canBuildRobot(type, Data.directions[i]) == true){
-                return Data.directions[i];
-            }
-        }
-        if (radialDirectionIndex != 0){
-            for (int i = 0; i < radialDirectionIndex; i++){
-                if (rc.canBuildRobot(type, Data.directions[i]) == true){
-                    return Data.directions[i];
+    public static Direction getSoldierSpawnDir(RobotController rc)throws GameActionException{
+        MapLocation closest = null;
+        for (int i = 0; i < 12; i++){
+            if (rc.readSharedArray(i) != 0){
+                MapLocation loc = Data.readMapLocationFromSharedArray(rc, rc.readSharedArray(i));
+                if (closest == null){
+                    closest = loc;
+                } else if (rc.getLocation().distanceSquaredTo(loc) < rc.getLocation().distanceSquaredTo(closest)){
+                    closest = loc;
                 }
             }
         }
-        return getRadialSpawnDir(rc, RobotType.MINER);
+        return Pathfinding.basicBuild(rc, closest, RobotType.SOLDIER);
+    }
+
+    public static void checkForMinerPhaseEnd(RobotController rc) throws GameActionException{
+        minerPhaseEnd = true;
+        forLoop:
+        for (int i = 41; i < 48; i+=2){
+            if (rc.readSharedArray(i) == rc.getID()){
+                if (rc.readSharedArray(i+1) != 1){
+                    rc.writeSharedArray(i+1, 1);
+                }
+                break forLoop;
+            }
+        }
     }
 
     public static void addPossibleEnemyArchonLocations(RobotController rc, int buffer) throws GameActionException{
@@ -129,16 +220,15 @@ public class Archon {
 
     public static void init(RobotController rc) throws GameActionException{
         nearbyLeadLocations = rc.senseNearbyLocationsWithLead().length;
-        maxMinerSpawns = nearbyLeadLocations + defaultMinerNumber;
-        firstMinerPhaseEnd = nearbyLeadLocations + defaultMinerNumber;
-        if (firstMinerPhaseEnd > maxMinerSpawns){
-            firstMinerPhaseEnd = maxMinerSpawns;
+        int temp = nearbyLeadLocations + defaultMinerNumber;
+        if (temp < maxMinerSpawns){
+            maxMinerSpawns = temp;
         }
 
         //check if enemy base is within sensing range of archon
         forLoop:
         for(RobotInfo robot: rc.senseNearbyRobots(34, rc.getTeam().opponent())){
-            if (robot.getType() == RobotType.ARCHON && robot.getTeam() != rc.getTeam()){
+            if (robot.getType() == RobotType.ARCHON){
                 enemyArchonNearby = true;
                 nearbyEnemyArchonLocation = robot.getLocation();
                 break forLoop;
@@ -170,13 +260,41 @@ public class Archon {
         } else{
             addPossibleEnemyArchonLocations(rc, 9);
         }
-        /**
-         * print to check guessed locations!
-        System.out.print(rc.getID());
-        for(int i = 0;i <12; i++){
-            System.out.print("( " +rc.readSharedArray(i) + " )");
+
+        //add id to 56-63 for explosive turtle checking
+        forLoop:
+        for (int i = 56; i < 63; i+=2){
+            if (rc.readSharedArray(i) == 0){
+                rc.writeSharedArray(i, rc.getID());
+                break forLoop;
+            }
         }
-        **/
+
+        //add id to 41-48 for soldier scouts
+        forLoop:
+        for (int i = 41; i < 48; i+=2){
+            if (rc.readSharedArray(i) == 0){
+                rc.writeSharedArray(i, rc.getID());
+                break forLoop;
+            }
+        }
+
+        leadBeforeBuild = 75*rc.getArchonCount();
+
+        int area = rc.getMapHeight()*rc.getMapWidth();
+        if (area <900){
+            soldiersInCircle = 10;
+        } else if (area < 1600){
+            soldiersInCircle = 15;
+        } else if (area < 2500){
+            soldiersInCircle = 20;
+        } else{
+            soldiersInCircle = 25;
+        }
+
+        //set randoms
+        rc.writeSharedArray(38, Data.rng.nextInt(4));
+        rc.writeSharedArray(38, Data.rng.nextInt(3));
     }
 }
 
--- src/ExplosiveTurtle/Miner.java
+++ src/ExplosiveTurtle/Miner.java
@@ -10,6 +10,10 @@ public class Miner {
     public static boolean foundLeadLocation = false;
     public static MapLocation targetLocation;
     public static MapLocation[] enemyArchons = new MapLocation[4];
+    public static MapLocation baseLoc;
+    public static MapLocation randomMapLocation;
+
+    public static int baseID;
 
     //miner that reads from the lead loacation array in reverse order
     public static boolean reverseMiner = false;
@@ -40,39 +44,22 @@ public class Miner {
                     break forLoop;
                 }
             }
-        }
+        } 
 
-        //finds a lead location to target for mining if one is not already found
-        MapLocation prioritizedTarget = null;
-        if (!foundLeadLocation){
-            Direction[] facingArray = Pathfinding.getFacingArray(rc, directionIndex);
-            forLoop:
-            for (MapLocation loc : leadLocations){
-                if (rc.canSenseLocation(loc)){
-                    if ((rc.senseLead(loc) > 1) && (rc.senseRobotAtLocation(loc) == null)){
-                        for (Direction dir : facingArray){
-                            if (rc.getLocation().directionTo(loc) == dir){
-                                prioritizedTarget = loc;
-                                System.out.println("(" + targetLocation.x + "' " + targetLocation.y + "), " + rc.getID());
-                                foundLeadLocation = true;
-                                break forLoop; 
-                            } else{
-                                targetLocation = loc;
-                            }
-                        }        
-                    }
-                }
+        //checks for nearby enemy archons and adds if it sees them
+        RobotInfo[] robots = rc.senseNearbyRobots();
+        for (RobotInfo robot : robots){
+            if (robot.type == RobotType.ARCHON && robot.team != rc.getTeam()){
+                addArchonToSharedArray(rc, robot.getLocation());
             }
         }
-        if (prioritizedTarget != null){
-            targetLocation = prioritizedTarget;
-        }   
+
+        setMiningTarget(rc, leadLocations);
 
         //pathfinds a direction to the target location
         Direction dir = Pathfinding.basicMove(rc, targetLocation);
 
         //if enemy robot nearby then run away and reset lead location target
-        RobotInfo[] robots = rc.senseNearbyRobots();
         for(RobotInfo robot: robots){
             if (robot.getType() == RobotType.SOLDIER && robot.getTeam() != rc.getTeam()){
                 if (rc.canMove(rc.getLocation().directionTo(robot.location).opposite())){
@@ -83,72 +70,120 @@ public class Miner {
         }
 
         //mines lead or moves towards target
-        if (rc.canSenseLocation(targetLocation)){
-            //mines until it cannot mine anymore spaces around it
-            while (rc.senseLead(targetLocation) > 1 && rc.canMineLead(targetLocation)){
-                MapLocation me = rc.getLocation();
-                for (int dx = -1; dx <= 1; dx++) {
-                    for (int dy = -1; dy <= 1; dy++) {
-                        MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
-                        // Notice that the Miner's action cooldown is very low.
-                        // You can mine multiple times per turn!
-                        while (rc.canMineGold(mineLocation)){ //gold doesnt regen
-                            rc.mineGold(mineLocation);
-                        }
-                        while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > 1) {
-                            rc.mineLead(mineLocation);
+        if (targetLocation != null){
+            if (rc.canSenseLocation(targetLocation)){
+                //mines until it cannot mine anymore spaces around it
+                while (rc.senseLead(targetLocation) > 1 && rc.canMineLead(targetLocation)){
+                    MapLocation me = rc.getLocation();
+                    for (int dx = -1; dx <= 1; dx++) {
+                        for (int dy = -1; dy <= 1; dy++) {
+                            MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                            // Notice that the Miner's action cooldown is very low.
+                            // You can mine multiple times per turn!
+                            while (rc.canMineGold(mineLocation)){ //gold doesnt regen
+                                rc.mineGold(mineLocation);
+                            }
+                            while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > 1) {
+                                rc.mineLead(mineLocation);
+                            }
                         }
                     }
+                    if (rc.senseLead(targetLocation) < 2){
+                        foundLeadLocation = false;
+                    }
                 }
-                if (rc.senseLead(targetLocation) < 2){
-                    foundLeadLocation = false;
+                //if not adjacent to target move towards the target
+                if (rc.canMove(dir) && foundLeadLocation && !rc.getLocation().isAdjacentTo(targetLocation)){
+                    rc.move(dir);   
                 }
-            }
-            //if not adjacent to target move towards the target
-            if (rc.canMove(dir) && foundLeadLocation && !rc.getLocation().isAdjacentTo(targetLocation)){
-                rc.move(dir);   
+            } else{
+                foundLeadLocation = false;
             }
         } else{
             foundLeadLocation = false;
         }
-
-        //move in default location if no lead locations found
+        //setting a random location
+        if(!foundLeadLocation){
+            if(rc.canSenseLocation(randomMapLocation)){
+                randomMapLocation = Pathfinding.randomMapLocation(rc);
+            }
+        }
+        //move to random location if no lead locations found
+        rc.setIndicatorString("I am moving to " + randomMapLocation);
         if (!foundLeadLocation){
-            Direction move = Pathfinding.getSemiRandomDir(rc, directionIndex);
+            Direction move = Pathfinding.basicMove(rc,randomMapLocation);
             if (rc.canMove(move)){
                 rc.move(move);
             }
         }
     }
 
+    public static void setMiningTarget(RobotController rc, MapLocation[] leadLocations) throws GameActionException{
+                //finds a lead location to target for mining if one is not already found
+                MapLocation prioritizedTarget = null;
+                if (!foundLeadLocation){
+                    for (MapLocation loc : leadLocations){
+                        if (rc.canSenseLocation(loc)){
+                            if ((rc.senseLead(loc) > 1)){
+                                if (prioritizedTarget == null){
+                                    prioritizedTarget = loc;
+                                    foundLeadLocation = true;
+                                } else if (rc.getLocation().distanceSquaredTo(loc) < rc.getLocation().distanceSquaredTo(prioritizedTarget)){
+                                    prioritizedTarget = loc;
+                                }  
+                            }
+                        }
+                    }
+                    targetLocation = prioritizedTarget;
+                }
+                if (prioritizedTarget != null){
+                    targetLocation = prioritizedTarget;
+                }  
+    }
+
+    //adds enemy archon to 12-15 in shared array
+    public static void addArchonToSharedArray(RobotController rc, MapLocation loc) throws GameActionException{
+        int intLocation = loc.x*100 + loc.y;
+        forLoop:
+        for (int i = 12; i < 16; i++){
+            if (rc.readSharedArray(i) == 0 && rc.readSharedArray(i) != intLocation){
+                rc.writeSharedArray(i, intLocation);
+                break forLoop;
+            } else if (rc.readSharedArray(i) == intLocation){
+                break forLoop;
+            }
+        }
+    }  
+
     public static void init(RobotController rc) throws GameActionException{
         //nearby bots
         RobotInfo[] robots = rc.senseNearbyRobots();
         //current MapLocation
         MapLocation loc = rc.getLocation();
+        //random MapLocation
+        randomMapLocation = Pathfinding.randomMapLocation(rc);
         //set default target
         targetLocation = loc;
-
-        MapLocation baseLoc = rc.getLocation();
 
         //find base and set move to away from base
         for(RobotInfo robot: robots){
             if (robot.getType() == RobotType.ARCHON && robot.getTeam() == rc.getTeam()){
-                MapLocation base = robot.getLocation();
-                baseLoc = base;
-                move = base.directionTo(loc);
+                baseLoc = robot.getLocation();
+                move = baseLoc.directionTo(rc.getLocation());
             }
         }
 
         //choose whether to reverse targeting order for lead (favoring away from the corners for more mining)
-        int rand = Data.rng.nextInt(3);
-        if (baseLoc.x < rc.getMapWidth()/2.0){
-            if (rand == 0){
-                reverseMiner = true;
-            }
-        } else{
-            if (rand != 0){
-                reverseMiner = true;
+        if (baseLoc != null){
+            int rand = Data.rng.nextInt(3);
+            if (baseLoc.x < rc.getMapWidth()/2.0){
+                if (rand == 0){
+                    reverseMiner = true;
+                }
+            } else{
+                if (rand != 0){
+                    reverseMiner = true;
+                }
             }
         }
 
